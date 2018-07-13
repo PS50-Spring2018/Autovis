5d9721c02c0f4045e886eeace51fc4b968b0174e
Santiago Vargas <santiagovargas921@gmail.com>
2018-07-13 13:29:11 -0400

    more comments

--- csvSave.py
+++ csvSave.py
@@ -1,5 +1,12 @@
 import csv as csv
     
+
+"""
+vars:
+
+Functionality: saves csv file, writes statistics to it. Potentially just add this function
+to the processor class.
+"""
 def save(rxnID, file, mean, variance, folderwoID):
     
     with open(folderwoID+'/summary_%s.csv' % (rxnID),'a+') as csvfile:
